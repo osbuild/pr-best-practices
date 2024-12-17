010f2367e8723bc19bdbd77ae22a8279e13511c0
Florian Schüller <florian.schueller@redhat.com>
2024-12-17 10:36:39 +0100

    pr_best_practices.py: fix tests

--- test_pr_best_practices.py
+++ test_pr_best_practices.py
@@ -1,4 +1,5 @@
 import unittest
+from contextlib import redirect_stdout
 from unittest.mock import patch
 import io
 import sys
@@ -19,32 +20,50 @@ class TestPRChecks(unittest.TestCase):
         """
         Test whether the function correctly identifies Jira ticket references in PR titles.
         """
-        self.assertTrue(check_pr_title_contains_jira("PR-123: Fix some issues"))
-        self.assertFalse(check_pr_title_contains_jira("Fix some issues"))
+
+        # None as a return just means that this does not sys.exit()…
+        self.assertIsNone(check_pr_title_contains_jira("myfile.py: Fix some issues (PR-123)"))
+
+        output = io.StringIO()
+        with redirect_stdout(output):
+            # the next call actually does a system exit, so we'll better be prepared
+            with self.assertRaises(SystemExit) as cm:
+                check_pr_title_contains_jira("Fix some issues")
+        self.assertEqual(cm.exception.code, 2)
+        self.assertIn("The pull request title should follow this schema", output.getvalue())
 
     def test_check_pr_title_contains_jira_empty(self):
         """
         Test whether the function correctly handles an empty PR title.
         """
-        self.assertFalse(check_pr_title_contains_jira(""))
+        with self.assertRaises(SystemExit) as cm:
+            self.assertFalse(check_pr_title_contains_jira(""))
+        self.assertEqual(cm.exception.code, 2)
 
     @patch('os.popen')
     def test_check_commits_contain_jira(self, mock_popen):
         """
         Test whether the function correctly identifies commits lacking Jira ticket references.
         """
-        mock_popen.return_value.read.return_value = "PR-123: Fix some issues\nTest commit\n"
+        # checking for empty message
+        mock_popen.return_value.read.return_value = "myfile.py: Fix some issues (PR-123)\n \nTest commit\n"
         with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            check_commits_contain_jira()
+            with self.assertRaises(SystemExit) as cm:
+                check_commits_contain_jira("HEAD")
+            self.assertEqual(cm.exception.code, 2)
             output = fake_stdout.getvalue().strip()
-            self.assertEqual(output, "Commit message 'Test commit' should contain a Jira.")
+            self.assertIn("Found empty commit message.", output)
 
     def test_check_pr_description_not_empty(self):
         """
         Test whether the function correctly identifies an empty PR description.
         """
-        self.assertTrue(check_pr_description_not_empty("This is a PR description"))
-        self.assertFalse(check_pr_description_not_empty(""))
+        # None as a return just means that this does not sys.exit()…
+        self.assertIsNone(check_pr_description_not_empty("This is a PR description"))
+
+        with self.assertRaises(SystemExit) as cm:
+            self.assertFalse(check_pr_description_not_empty(""))
+        self.assertEqual(cm.exception.code, 1)
 
     @patch('requests.post')
     def test_add_best_practice_label(self, mock_post):
@@ -58,6 +77,7 @@ class TestPRChecks(unittest.TestCase):
             add_best_practice_label("mock_token", "mock_repo", 123)
             output = fake_stdout.getvalue().strip()
             self.assertEqual(output, "Label 'best-practice' added to PR successfully.")
+
 
 if __name__ == '__main__':
     unittest.main()
